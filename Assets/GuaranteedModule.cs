4f84a285443d332bc9f72268a4eed640bf2e7255
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2021-12-13 22:16:47 -0500

    [V2.1] Updates

--- Assets/GuaranteedModule.cs
+++ Assets/GuaranteedModule.cs
@@ -1,106 +1,175 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
+using System;
+using System.Reflection;
 
 public class GuaranteedModule : MonoBehaviour
 {
-    public KMSelectable Button, Parent, BottomFaceSel;
+    public KMSelectable LButton, RButton, Parent, BottomFaceSel;
     public Transform[] Anchors;
+    public int AnchorsLength;
+    public Transform Divider;
     public KMAudio Audio;
 
-    private Transform _cover, _topFace, _bottomFace;
-    private int _animState;
+    private Transform _cover, _topFace, _bottomFace, _bonusAnchor;
+    private int _animState, _currentPage;
     private Dictionary<Transform, Transform> _modules = new Dictionary<Transform, Transform>();
+    private List<List<Transform>> _pages;
+    private List<bool> _emptyPages;
 
     private void Awake()
     {
-        _cover = transform.GetChild(0).GetChild(6);
+        _pages = Anchors.Take(Anchors.Length - 1).SplitLength(AnchorsLength).Select(e => e.ToList()).ToList();
+        _emptyPages = Enumerable.Repeat(true, _pages.Count).ToList();
+        _bonusAnchor = Anchors.Last();
+        _bonusAnchor.gameObject.SetActive(false);
+
+        _cover = transform.GetChild(0).GetChild(7);
         _topFace = transform.GetChild(0).GetChild(3).GetChild(0);
         _bottomFace = transform.GetChild(0).GetChild(3).GetChild(1);
 
-#if !UNITY_EDITOR
         Type type = Type.GetType("ExcludeFromStaticBatch, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
         transform.GetChild(0).gameObject.AddComponent(type);
-#endif
     }
 
     private void Start()
     {
         StartCoroutine(Init());
 
-        Button.OnInteract += delegate () {
+        LButton.OnInteract += delegate ()
+        {
             Debug.Log("[Cursed Casings] Casing button pressed.");
-            StartCoroutine(Animate());
-            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.WireSequenceMechanism, Button.transform);
+            StartCoroutine(Animate(false));
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.WireSequenceMechanism, LButton.transform);
+            return false;
+        };
+
+        RButton.OnInteract += delegate ()
+        {
+            Debug.Log("[Cursed Casings] Casing button pressed.");
+            StartCoroutine(Animate(true));
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.WireSequenceMechanism, RButton.transform);
             return false;
         };
     }
 
-    private IEnumerator Animate()
+    private IEnumerator Animate(bool forwards)
     {
-        if(_animState == 1 || _animState == 3)
+        if(_animState == 1)
             yield break;
-        _animState++;
-        bool forwards = _animState == 1;
+        _animState = 1;
+
+        int prevPage = _currentPage;
+        do
+        {
+            _currentPage += forwards ? 1 : _pages.Count - 1;
+            _currentPage %= _pages.Count;
+        }
+        while(_emptyPages[_currentPage] == true);
 
         float time = Time.time;
-        while(Time.time - time < 1f)
+        while(Time.time - time < .5f)
         {
             _cover.gameObject.transform.localScale = new Vector3(Mathf.Max(0f, 1f - 4f * Mathf.Abs(Time.time - time - .5f)), 1f, 1f);
-            (forwards ? _topFace : _bottomFace).localPosition = new Vector3(0f, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 1f - 2f * (Time.time - time))), 0f);
-            (forwards ? _bottomFace : _topFace).localPosition = new Vector3(0f, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 2f * (Time.time - time) - 1f)), 0f);
-            foreach(KeyValuePair<Transform, Transform> kvp in _modules)
-            {
-                kvp.Value.position = kvp.Key.position;
-            }
+            _pages[prevPage].ForEach(t => t.localPosition = new Vector3(t.localPosition.x, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 1f - 2f * (Time.time - time))), t.localPosition.z));
+            _pages[_currentPage].ForEach(t => t.localPosition = new Vector3(t.localPosition.x, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 2f * (Time.time - time) - 1f)), t.localPosition.z));
+            Divider.localPosition = new Vector3(0f, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 2f * (Time.time - time) - 1f, 1f - 2f * (Time.time - time))), 0f);
+            UpdateAnchors();
 
             yield return null;
         }
 
-        (forwards ? _bottomFace : _topFace).localPosition = new Vector3(0f, .2f, 0f);
+        _pages[prevPage].ForEach(t => t.localScale = Vector3.zero);
+        _pages[_currentPage].ForEach(t => t.localScale = 1.2f * Vector3.one);
 
+        while(Time.time - time < 1f)
+        {
+            _cover.gameObject.transform.localScale = new Vector3(Mathf.Max(0f, 1f - 4f * Mathf.Abs(Time.time - time - .5f)), 1f, 1f);
+            _pages[prevPage].ForEach(t => t.localPosition = new Vector3(t.localPosition.x, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 1f - 2f * (Time.time - time))), t.localPosition.z));
+            _pages[_currentPage].ForEach(t => t.localPosition = new Vector3(t.localPosition.x, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 2f * (Time.time - time) - 1f)), t.localPosition.z));
+            Divider.localPosition = new Vector3(0f, Mathf.Lerp(0f, .2f, Mathf.Max(0f, 2f * (Time.time - time) - 1f, 1f - 2f * (Time.time - time))), 0f);
+            UpdateAnchors();
+
+            yield return null;
+        }
+
+        _pages[_currentPage].ForEach(t => t.localPosition = new Vector3(t.localPosition.x, .2f, t.localPosition.z));
+
+        UpdateAnchors();
+
+        _animState = 0;
+    }
+
+    private void UpdateAnchors()
+    {
         foreach(KeyValuePair<Transform, Transform> kvp in _modules)
         {
             kvp.Value.position = kvp.Key.position;
+            kvp.Value.localScale = kvp.Key.localScale;
         }
-
-        _animState++;
-        _animState %= 4;
     }
 
     private IEnumerator Init()
     {
+        /*
+#if !UNITY_EDITOR
+        Component state = FindObjectsOfType<Component>().Where(c => c.GetType().Name.Contains("GameplayState")).First();
+        PropertyInfo prop = state.GetType().GetProperty("Bomb", BindingFlags.Public | BindingFlags.Instance);
+        yield return new WaitUntil(() => prop.GetValue(state, new object[0]) != null);
+#endif
+        */
         yield return new WaitForSeconds(1f);
+        yield return null;
 
-        for(int i = transform.GetChild(0).childCount - 1; i > 6; i--)
+        _modules.Add(transform.GetChild(0).GetChild(3).GetChild(2), transform.GetChild(0).GetChild(9));
+
+        List<Transform> modules = new List<Transform>();
+        List<Transform> empties = new List<Transform>();
+
+        for(int i = transform.GetChild(0).childCount - 1; i > 9; i--)
         {
             Transform tr = transform.GetChild(0).GetChild(i);
-            tr.localScale = 1.2f * Vector3.one;
-            Transform p = Anchors.Where(a => (tr.position - a.position).magnitude < 0.0001f).FirstOrDefault();
-            if(p != null)
+            if(tr.gameObject.name.ContainsIgnoreCase("Empty"))
             {
-                _modules.Add(p, tr);
+                empties.Add(tr);
             }
             else
             {
-                throw new Exception("A module spawned in a weird place. Error!");
+                _emptyPages[(i - 10) / AnchorsLength] = false;
+                modules.Add(tr);
             }
         }
 
-#if !UNITY_EDITOR
+        modules = modules.Shuffle();
+
+        for(int i = 0; i < modules.Count; i++)
+            _modules.Add(Anchors[i], modules[i]);
+        for(int i = 0; i < empties.Count; i++)
+            _modules.Add(Anchors[i + modules.Count], empties[i]);
+
         Component obj = Parent.GetComponents<Component>().Where(c => c.GetType().Name.Contains("ModSelectable") && !c.GetType().Name.Contains("KM")).First();
-        Component buttonObj = Button.GetComponents<Component>().Where(c => c.GetType().Name.Contains("ModSelectable") && !c.GetType().Name.Contains("KM")).First();
+        Component buttonObj = LButton.GetComponents<Component>().Where(c => c.GetType().Name.Contains("ModSelectable") && !c.GetType().Name.Contains("KM")).First();
+        Component button2Obj = RButton.GetComponents<Component>().Where(c => c.GetType().Name.Contains("ModSelectable") && !c.GetType().Name.Contains("KM")).First();
         Component bottomObj = _bottomFace.GetComponents<Component>().Where(c => c.GetType().Name.Contains("ModSelectable") && !c.GetType().Name.Contains("KM")).First();
         FieldInfo fld = obj.GetType().GetField("Children", BindingFlags.Instance | BindingFlags.Public);
         Array initial = (Array)fld.GetValue(obj);
         Array bottomInitial = (Array)fld.GetValue(bottomObj);
-        Array final = Array.CreateInstance(fld.FieldType.GetElementType(), initial.Length + 1 + bottomInitial.Length);
+        Array final = Array.CreateInstance(fld.FieldType.GetElementType(), initial.Length + 3 + bottomInitial.Length);
         initial.CopyTo(final, 0);
-        bottomInitial.CopyTo(final, initial.Length + 1);
+        bottomInitial.CopyTo(final, initial.Length + 3);
         final.SetValue(buttonObj, initial.Length);
+        final.SetValue(null, initial.Length + 1);
+        final.SetValue(button2Obj, initial.Length + 2);
+
+        FieldInfo fldParent = obj.GetType().GetField("Parent", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+
+        foreach(object mod in initial)
+        {
+            if(mod != null)
+                fldParent.SetValue(mod, obj);
+        }
 
         obj.GetType().GetMethod("DeactivateImmediateChildSelectableAreas", BindingFlags.Instance | BindingFlags.Public).Invoke(obj, new object[0]);
         bottomObj.GetType().GetMethod("DeactivateImmediateChildSelectableAreas", BindingFlags.Instance | BindingFlags.Public).Invoke(obj, new object[0]);
@@ -108,7 +177,13 @@ public class GuaranteedModule : MonoBehaviour
         fld.SetValue(obj, final);
         obj.GetType().GetMethod("Init", BindingFlags.Instance | BindingFlags.Public).Invoke(obj, new object[0]);
         bottomObj.GetType().GetMethod("Init", BindingFlags.Instance | BindingFlags.Public).Invoke(obj, new object[0]);
-#endif
+
+        yield return null;
+
+        Enumerable.Range(1, _pages.Count - 1).ToList().ForEach(j => _pages[j].ForEach(t => { t.localScale = Vector3.zero; t.localPosition = new Vector3(t.localPosition.x, 0f, t.localPosition.z); }));
+        _pages[0].ForEach(t => { t.localScale = 1.2f * Vector3.one; t.localPosition = new Vector3(t.localPosition.x, .2f, t.localPosition.z); });
+        UpdateAnchors();
+
         /*
         yield return new WaitForSecondsRealtime(20f);
         PrintHierarchy(1);
@@ -138,5 +213,19 @@ public class GuaranteedModule : MonoBehaviour
     {
         return o.GetType().GetFields().Select(m => m.Name + ":" + m.ReflectedType + ":" + m.GetValue(o)).Join("/");
     */
+    }
+}
+
+public static class Ex
+{
+    public static IEnumerable<IEnumerable<T>> SplitLength<T>(this IEnumerable<T> obj, int len)
+    {
+        int i;
+        for(i = 0; i < obj.Count() - len; i += len)
+        {
+            yield return obj.Skip(i).Take(len);
+        }
+        if(obj.Skip(i).Count() > 0)
+            yield return obj.Skip(i);
     }
 }
